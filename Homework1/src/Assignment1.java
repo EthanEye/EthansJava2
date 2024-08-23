084a006707f81e677bf10a8e9fa6d674fb61aa9b
Ethan Eye <ethaneye02@gmail.com>
2024-08-22 23:08:31 -0600

    Assignment 1

---
+++ Homework1/src/Assignment1.java
@@ -0,0 +1,101 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//Credit card algorithm (Hans Luhn)
+//valid 4388576018410707
+//not valid 4388576018402626
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Credit Card Number...");
+
+		while (true) {
+			try {
+				Long cardNum = input.nextLong();
+				String cardString = cardNum.toString();
+
+				if (cardString.length() == 16) {
+					int sum = sumOfDoubleEvenPlace(cardNum);
+					int oddSum = sumOfOddPlace(cardNum);
+					System.out.println(isValid(sum, oddSum));
+					break;
+				} else {
+					System.out.println("Must be 16 digits...");
+					continue;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Enter a Number...");
+				input.next();
+			}
+		}
+
+	}
+
+	private static int sumOfDoubleEvenPlace(Long cardString) {
+		long cardNum = (long) cardString;
+		int digit = 0;
+		int sum = 0;
+		for (int i = 15; i >= 0; i--) {
+			// traverses backwards if index is even digit is multiplied by doubleNum value
+			// if its less than 10 its immediately multiplied than added to sum
+
+			if (i % 2 == 0 || i == 0) {
+				digit = (int) (cardNum % 10);
+				System.err.println(digit);
+				sum = sum + getDigit(digit);
+				cardNum = cardNum / 10; // remove a number each pass through the loop
+
+			} else {
+				cardNum = cardNum / 10;
+			}
+		}
+		System.out.println(sum);
+		return sum;
+	}
+
+	private static int getDigit(int digit) {
+		int sum = 0;
+		if (2 * digit < 10) {
+			sum = sum + (2 * digit);
+		} else { // else it will take the first number and the last number add them together a
+					// then
+					// add it to the sum
+			sum = sum + ((2 * digit) / 10) + ((2 * digit) % 10);
+
+		}
+
+		return sum;
+	}
+
+	private static int sumOfOddPlace(Long cardNum) {
+		long cardNumOdd = (long) cardNum;
+		int digitOdd = 0;
+		int oddSum = 0;
+		for (int i = 15; i >= 0; i--) {
+			if (i % 2 != 0) {
+				digitOdd = (int) (cardNumOdd % 10);
+				oddSum = oddSum + digitOdd;
+				cardNumOdd = cardNumOdd / 10;
+			} else {
+				cardNumOdd = cardNumOdd / 10;
+
+			}
+
+		}
+
+		return oddSum;
+	}
+
+	private static String isValid(int sum, int oddSum) {
+		String valid = null;
+
+		if ((sum + oddSum) % 10 == 0) {
+			valid = "Valid";
+		} else {
+			valid = "Not valid";
+		}
+		return valid;
+	}
+
+}
--- Homework1/src/JavaApplications.java
+++
@@ -1,9 +0,0 @@
-
-public class JavaApplications {
-
-	public static void main(String[] args) {
-		
-
-	}
-
-}
