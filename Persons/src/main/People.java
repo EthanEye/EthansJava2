78d6d69d183adbbd0939e4cb5d4a5e9eacea7029
Eye <S02584355@ccc.ccofc.edu>
2024-08-27 14:51:14 -0600

    People

---
+++ Persons/src/main/People.java
@@ -0,0 +1,14 @@
+package main;
+
+public class People {
+
+	public static void main(String[] args) {
+	Person ethan = new Person("Ethan", "Eye", 22);
+	Person rob = new Person();
+	System.out.println(ethan.toString());
+	System.out.println(rob.toString());
+}
+	
+	
+
+}
